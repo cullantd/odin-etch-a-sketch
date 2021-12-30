f0b5d5af84e29fe251d1a39224c599c19ccd6c1f
Cullan Diffee <cullandiffee@gmail.com>
2021-12-30 14:52:26 +0800

    fix creation of grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,18 @@
 const container = document.querySelector('#div-container');
-const newButton = document.querySelector('#new-button');
 
 //create a 256-div grid (16x16)
-for (let i = 0; i < 256; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.setAttribute("class", "grid-div");
-    container.appendChild(gridDiv);
+function createGrid(numSquares) {
+    let totalNumSquares = Math.pow(numSquares, 2);
+    for (let i = 0; i < totalNumSquares; i++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.setAttribute("class", "grid-div");
+        container.appendChild(gridDiv);
+    }
 }
 
-//code for drawing
-const divs = container.querySelectorAll('div'); //nodelist of all divs (grid squares) within the container div
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "black";
-    });
-});
 
 //code to clear grid when reset button is pressed
-newButton.addEventListener("click", () => {
-    divs.forEach((div) => {
-        div.style.backgroundColor = "white"; //erase the drawing
-    });
+function resetNewGrid() {
 
     let numSquares = prompt("Enter the number of squares per side for the new grid (must be 100 or less)", "16");
     let cancelText;
@@ -29,12 +21,33 @@ newButton.addEventListener("click", () => {
     } else if (numSquares > 100 || numSquares < 0) {
         cancelText = "Invalid number;"
     } else {
+        createGrid(numSquares);
         let numFractionsString ="";
         for (let j = 0; j < numSquares; j++) {
             numFractionsString = numFractionsString.concat(" 1fr");
         }
         container.style.gridTemplateColumns = numFractionsString;
         container.style.gridTemplateRows = numFractionsString;
-    }
 
-});
+        let divs = container.querySelectorAll('div'); //nodelist of all divs (grid squares) within the container div
+
+        divs.forEach((div) => {
+            div.style.backgroundColor = "white"; //erase the drawing
+        });
+
+        draw(divs);
+
+    }
+}
+
+function draw(divs) {
+        //drawing
+        divs.forEach((div) => {
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "black";
+            });
+        });
+}
+
+//main (on load)
+resetNewGrid();
